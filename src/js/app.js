8a28eeb6e281ca39d5ff6d03fe29da7edfd98a6c
alex-mal79 <a-79maleev@yandex.ru>
2021-12-05 16:44:50 +0300

    Старт проекта "Cправочник для верстки"

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+window.addEventListener("load", function() {
+// Подключение функций
+@@include('functions/_ibg.js');
+@@include('functions/_burger.js');
+// Подключение плагинов
+});
